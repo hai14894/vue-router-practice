f9409352c2e21c7ad60069595346a169b09bfc7c
hai14894 <hohoanghai14894@gmail.com>
2020-09-20 14:03:07 +1000

    added projects page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Projects from '../views/Projects'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
   },
   {
     path: '/about',
---
+++ src/views/Projects.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>This is the projects page</div>
+</template>
